b7b447e4cfc12501740215179de67d29dbda5b43
Rob Bransby <rob@bransby.com.au>
2021-08-31 15:11:38 +0800

    added more test coverage for consecutive spares and strikes

--- test/bowlinggame.spec.ts
+++ test/bowlinggame.spec.ts
@@ -52,6 +52,31 @@ describe("BowlingGame", () => {
         const result = game.Score();
         expect(result).to.equal(28);
     });
+
+    it("Should return a score of 53 when 10 | 10 | 5,4 are rolled", () => {
+        const game = new BowlingGame();
+        game.Roll(10);        
+        game.Roll(10);        
+        game.Roll(5);
+        game.Roll(4);        
+
+        const result = game.Score();
+        expect(result).to.equal(53);
+    });
+
+    it("Should return a score of 53 when 10 | 5 5 | 5,5 | 4 3 are rolled", () => {
+        const game = new BowlingGame();
+        game.Roll(10);        
+        game.Roll(5);        
+        game.Roll(5);
+        game.Roll(5);        
+        game.Roll(5);
+        game.Roll(4);        
+        game.Roll(3);
+
+        const result = game.Score();
+        expect(result).to.equal(56);
+    });
     
     it("Should throw an error when a negative roll is passed in", () => {
         const game = new BowlingGame();                
